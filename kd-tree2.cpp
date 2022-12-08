1aab50d5e701d1e5f0dd94b986e606b0050a5f36
Omar Abdulla Ibrahim <ge75vum@mytum.de>
2022-12-08 23:22:25 +0100

    little clean up

--- kd-tree2.cpp
+++ kd-tree2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include<sstream>
-#include<string>
+#include <sstream>
+#include <string>
 #include <algorithm>
 
 /* ## Sprint 1
@@ -20,7 +20,8 @@ const int DIM = 2;
 // KDNode is a node or a leaf in KD-tree
 // the vec is its value
 // cuttingEdge is the x or y cutting edge that is currently using
-struct KDNode{
+struct KDNode
+{
     int cuttingEdge;
     std::vector<int> vector;
     KDNode *left;
@@ -31,130 +32,128 @@ struct KDNode{
 // insert and delete has a public and private function, it serves OOP purpose.
 class KDTree
 {
-    public:
-  
-        // Constructor
-        KDTree(){
-            root = NULL;
-        }
 
-        void insert(std::vector<int> vec){
-            if(root!=NULL) insert(vec, root);
-            else {
-                root= new KDNode;
-                root->cuttingEdge = 0;
-                root->vector = vec;
-                root->left=NULL;
-                root->right=NULL;
-            }
-        }
-
-        KDNode* findMin(int dim){
-            return findMin(dim, root);
-        }
-
- 
-    private:
-        void insert(std::vector<int> vec, KDNode *leaf){
-            if(vec[leaf->cuttingEdge]< leaf->vector[leaf->cuttingEdge]){
-                if(leaf->left!=NULL) insert(vec, leaf->left);
-                else {
-                    leaf->left=new KDNode;
-                    leaf->left->vector=vec;
-                    leaf->left->cuttingEdge = (leaf->cuttingEdge+1)%DIM;
-                    leaf->left->left=NULL;    //Sets the left child of the child node to null
-                    leaf->left->right=NULL;   //Sets the right child of the child node to null
-                }  
-            } else if(vec[leaf->cuttingEdge]>=leaf->vector[leaf->cuttingEdge]) {
-                if(leaf->right!=NULL) insert(vec, leaf->right);
-                else {
-                    leaf->right=new KDNode;
-                    leaf->right->vector=vec;
-                    leaf->right->cuttingEdge = (leaf->cuttingEdge+1)%DIM;
-                    leaf->right->left=NULL;  //Sets the left child of the child node to null
-                    leaf->right->right=NULL; //Sets the right child of the child node to null
-                }
-            }
-        }
-
-        KDNode* minOfPoints(std::vector<KDNode*> points, int dim)
+    KDNode* minOfPoints(std::vector<KDNode *> points, int dim)
+    {
+        KDNode *min_el = NULL;
+        for (auto point : points)
         {
-            KDNode* min_el = NULL;
-            for (auto el : points)
+            if (min_el == NULL)
             {
-                if(min_el == NULL) {
-                    min_el = el;
-                } else if(el != NULL) {
-                    if (el->vector[dim] < min_el->vector[dim]) {
-                        min_el = el;
-                    }
+                min_el = point;
+            }
+            else if (point != NULL)
+            {
+                if (point->vector[dim] < min_el->vector[dim])
+                {
+                    min_el = point;
                 }
             }
-            return min_el;
         }
+        return min_el;
+    }
 
-        KDNode* findMin(int dim, KDNode* leaf){
-            if (leaf == NULL)
-                return NULL;
+public:
+    // Constructor
+    KDTree()
+    {
+        root = NULL;
+    }
 
+    void insert(std::vector<int> vec)
+    {
+        if (root != NULL)
+            insert(vec, root);
+        else
+        {
+            root = new KDNode;
+            root->cuttingEdge = 0;
+            root->vector = vec;
+            root->left = NULL;
+            root->right = NULL;
+        }
+    }
 
-            if(dim == leaf->cuttingEdge)
-                {
-                    if(leaf->left == NULL) 
-                        return leaf;
-                    else 
-                        return findMin(dim, leaf->left);
-                }
+    KDNode *findMin(int dim)
+    {
+        return findMin(dim, root);
+    }
+
+private:
+    void insert(std::vector<int> vec, KDNode *leaf)
+    {
+        if (vec[leaf->cuttingEdge] < leaf->vector[leaf->cuttingEdge])
+        {
+            if (leaf->left != NULL)
+                insert(vec, leaf->left);
             else
-                {
-
-                    return this->minOfPoints({findMin(dim, leaf->right), findMin(dim, leaf->left), leaf}, dim);
-                }
+            {
+                leaf->left = new KDNode;
+                leaf->left->vector = vec;
+                leaf->left->cuttingEdge = (leaf->cuttingEdge + 1) % DIM;
+                leaf->left->left = NULL;  // Sets the left child of the child node to null
+                leaf->left->right = NULL; // Sets the right child of the child node to null
+            }
         }
+        else if (vec[leaf->cuttingEdge] >= leaf->vector[leaf->cuttingEdge])
+        {
+            if (leaf->right != NULL)
+                insert(vec, leaf->right);
+            else
+            {
+                leaf->right = new KDNode;
+                leaf->right->vector = vec;
+                leaf->right->cuttingEdge = (leaf->cuttingEdge + 1) % DIM;
+                leaf->right->left = NULL;  // Sets the left child of the child node to null
+                leaf->right->right = NULL; // Sets the right child of the child node to null
+            }
+        }
+    }
 
-        // root of the tree
-        KDNode *root;
+    KDNode *findMin(int dim, KDNode *leaf)
+    {
+        if (leaf == NULL)
+            return NULL;
 
+        if (dim == leaf->cuttingEdge)
+        {
+            if (leaf->left == NULL)
+                return leaf;
+            else
+                return findMin(dim, leaf->left);
+        }
+        else
+        {
 
+            return KDTree::minOfPoints({findMin(dim, leaf->right), findMin(dim, leaf->left), leaf}, dim);
+        }
+    }
 
+    // root of the tree
+    KDNode *root;
 };
 
-void createFromFile(std::string file_name, KDTree *tree) {
+void createFromFile(std::string file_name, KDTree *tree)
+{
     std::string line;
-    std::ifstream my_file (file_name);
-    int x,y;
-    while (std::getline(my_file, line)) 
+    std::ifstream my_file(file_name);
+    int x, y;
+    while (std::getline(my_file, line))
     {
         std::replace(line.begin(), line.end(), ',', ' ');
         std::stringstream line_stream(line);
-        
+
         line_stream >> x >> y;
-        tree->insert({x,y});
-    } 
+        tree->insert({x, y});
+    }
 }
 
 int main()
 {
     KDTree *kdt = new KDTree;
-    // kdt->insert({3,4});
-    // kdt->insert({2,3});
-    // // kdt->insert({3,5});
-
     createFromFile("input.csv", kdt);
-    // std::cout << kdt << std::endl;
-    //std::cout<<kdt->root->vector[0]<<": was the root\n";
-    //std::cout<<kdt->root->left->vector[1]<<": was the root\n";
-    //std::cout<<kdt->root->right->vector[1]<<": was the root\n";
-    auto x = kdt->findMin(1);
 
-
-/*    
-    
-    bt->insert(3);
-    std::cout<<bt->search(2)->right->key_value<<": was the right\n";
-    bt->insert(3);
-    std::cout<<bt->search(2)->right->right->key_value<<": was the right right \n";
-    bt->insert(2);
-    std::cout<<bt->search(2)->right->left->key_value<<": was wrong\n";
-    return 0; */
+    int dim = 0;
+    auto x = kdt->findMin(dim);
+    std::cout << "min Point on dimension " << dim <<": [" << x->vector[0] << "," << x->vector[1] << "]" << "\n";
 }
